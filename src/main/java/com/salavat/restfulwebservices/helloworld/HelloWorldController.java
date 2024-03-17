3abd8588d802261829f51ca24ea5f6484981792c
Salavat <salavat.islamov.96@gmail.com>
2024-03-17 11:24:09 +0100

    Internalazation using i18n

--- src/main/java/com/salavat/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/salavat/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,11 +1,19 @@
 package com.salavat.restfulwebservices.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -20,6 +28,12 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean("Hello World " + name);
+	}
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 
 }
