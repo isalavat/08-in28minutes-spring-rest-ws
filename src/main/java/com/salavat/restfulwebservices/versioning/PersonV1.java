1822ca6b9c70fe8433fc2517faa6fbc0dafe54dc
Salavat <salavat.islamov.96@gmail.com>
2024-03-17 11:32:09 +0100

    URI Versioning

---
+++ src/main/java/com/salavat/restfulwebservices/versioning/PersonV1.java
@@ -0,0 +1,15 @@
+package com.salavat.restfulwebservices.versioning;
+
+public class PersonV1 {
+	private String name;
+
+	public PersonV1(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
